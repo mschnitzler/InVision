76dd9ef2672ec26a7c369b8e8cefeffec23fd979
Kimathi <S4Kingsten@gmail.com>
2017-07-07 09:43:12 +0200

    Changed BASE_URL

--- app/src/main/java/com/kimathi/rfid/invision/InvisionController.java
+++ app/src/main/java/com/kimathi/rfid/invision/InvisionController.java
@@ -15,7 +15,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class InvisionController  {
-    static final String BASE_URL = "";
+    static final String BASE_URL = "Test";
     public Gson gson;
     public Retrofit retrofit;
     public ActivityResource activityResource;
